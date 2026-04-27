831d6e9aa2d93b70d3cdb15d1e13844b682f671d
hllverel <hllozdemir@live.com>
2026-04-28 02:05:35 +0300

    css update

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -1,6 +1,6 @@
 import { iconSpritePath } from '../../constants/assets.js';
 
-function Icon({ name, className = '', width = 24, height = 24 }) {
+function Icon({ name, className = '', width, height }) {
   return (
     <svg className={className} width={width} height={height} aria-hidden="true">
       <use href={`${iconSpritePath}#icon-${name}`} />
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,36 +1,30 @@
 import { NavLink } from 'react-router-dom';
 import { ROUTES } from '../../constants/routes.js';
-import css from './Navigation.module.css'
-import icons from '../../assets/icons/navigation-icons.svg'
+import './Navigation.css';
+import Icon from '../Icon/Icon.jsx';
 
 const buildNavClassName = ({ isActive }) =>
-  `navigation__link${isActive ? 'navigation__link--active' : ''}`;
+  `navigation__link${isActive ? ' navigation__link--active' : ''}`;
 
 // const buildSpanClassName = ({ isActive }) =>
 //   `navspan${isActive ? 'navspan--active' : ''}`;
 
 export default function Navigation() {
   return (
-    <nav className={css.navigation}>
+    <nav className="navigation">
       <NavLink className={buildNavClassName} to={`${ROUTES.DASHBOARD}${ROUTES.HOME}`}>
-        <svg className={css.navicon}>
-          <use href={`${icons}#navlink-home`}></use>
-        </svg>
-        <span className={css.navspan}>Home</span>
+        <Icon className="navicon" name="home" />
+        <span className="navspan">Home</span>
       </NavLink>
 
       <NavLink className={buildNavClassName} to={`${ROUTES.DASHBOARD}${ROUTES.STATISTICS}`}>
-        <svg className={css.navicon}>
-          <use href="../../assets/icons/navigation-icons.svg#navlink-statistics"></use>
-        </svg>
-        <span className={css.navspan}>Statistics</span>
+        <Icon className="navicon" name="statistics"/>
+        <span className="navspan">Statistics</span>
       </NavLink>
 
       <NavLink className={buildNavClassName} to={`${ROUTES.DASHBOARD}${ROUTES.CURRENCY}`}>
-        <svg className={css.navicon}>
-          <use href="../../assets/icons/navigation-icons.svg#navlink-currency"></use>
-        </svg>
-        <span className={css.navspan}>Currency</span>
+        <Icon className="navicon" name="currency"/>
+        <span className="navspan">Currency</span>
       </NavLink>
     </nav>
   );
