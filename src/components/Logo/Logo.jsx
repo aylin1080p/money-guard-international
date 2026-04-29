14c44c29372dc7f668ad2fa6d08b5b2383e3ced5
aylin <4ylingunes@gmail.com>
2026-04-29 20:06:20 +0200

    Fix logo home navigation

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,12 +1,19 @@
+import { Link } from 'react-router-dom';
+
+import { ROUTES } from '../../constants/routes.js';
+import { useAuth } from '../../hooks/useAuth.js';
 import Icon from '../Icon/Icon.jsx';
 import './Logo.css';
 
 function Logo() {
+  const { isLoggedIn } = useAuth();
+  const targetPath = isLoggedIn ? `${ROUTES.DASHBOARD}${ROUTES.HOME}` : ROUTES.LOGIN;
+
   return (
-    <a className="logo" href="/">
+    <Link className="logo" to={targetPath}>
       <Icon className="logo__icon" name="logo" width={28} height={34} />
       <span className="logo__text">Money Guard</span>
-    </a>
+    </Link>
   );
 }
 
